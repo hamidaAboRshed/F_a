9837eb2394c38170dfb04318b18f621b5f78e173
Hamida AboRshed <33187531+hamidaAboRshed@users.noreply.github.com>
2019-11-21 04:54:50 +0200

    add logo and some lib

---
+++ application/views/includes/header.php
@@ -0,0 +1,64 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo $title?></title>
+    <meta name="description" content="FAHESWebApplication">
+    <meta name="author" content="HamidaAboRshed">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="<?php echo base_url();?>/assets/images/logo/favicon.png">
+    <!-- Favicon icon -->
+    <link rel="shortcut icon" type="image/png" sizes="16x16" href="<?php echo base_url();?>/assets/images/logo/favicon.png">
+
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/normalize.min.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/icons/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/icons/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/icons/Icon-font-7-stroke-PIXEDEN-v-1.2.0/Pe-icon-7-stroke.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/icons/flag-icon-css/flag-icon.min.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
+     <script type="text/javascript" src="<?php echo base_url()?>assets/js/javaScript/html5shiv.min.js"></script> 
+    <link href="<?php echo base_url()?>assets/css/chartist.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/css/jqvmap.min.css" rel="stylesheet">
+
+    <link href="<?php echo base_url()?>assets/icons/weather-icons/css/weather-icons.min.css" rel="stylesheet" />
+    <link href="<?php echo base_url()?>assets/css/fullcalendar.min.css" rel="stylesheet" />
+
+   <style>
+    #weatherWidget .currentDesc {
+        color: #ffffff!important;
+    }
+        .traffic-chart {
+            min-height: 335px;
+        }
+        #flotPie1  {
+            height: 150px;
+        }
+        #flotPie1 td {
+            padding:3px;
+        }
+        #flotPie1 table {
+            top: 20px!important;
+            right: -10px!important;
+        }
+        .chart-container {
+            display: table;
+            min-width: 270px ;
+            text-align: left;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        #flotLine5  {
+             height: 105px;
+        }
+
+        #flotBarChart {
+            height: 150px;
+        }
+        #cellPaiChart{
+            height: 160px;
+        }
+
+    </style>
+</head>
---
+++ application/views/includes/left_panel.php
@@ -0,0 +1,110 @@
+<nav class="navbar navbar-expand-sm navbar-default">
+    <div id="main-menu" class="main-menu collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li class="active">
+                <a href="index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+            </li>
+            <li class="menu-title">Costumers Section</li><!-- /.menu-title -->
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>Costumers</a>
+                <ul class="sub-menu children dropdown-menu">                     
+                    <li><i class="fa fa-table"></i>
+                    
+                    <a href="tables-data.html">Costumer1</a></li>
+                    <li><i class="fa fa-id-badge"></i><a href="ui-badges.html">costum2</a></li>
+                    <li><i class="fa fa-bars"></i><a href="ui-tabs.html">costum2</a></li>
+                    
+                    
+                </ul>
+            </li>
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Orders</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="fa fa-table"></i><a href="<?php echo site_url('Activity/ActiveF')?>">order1</a></li>
+                    <li><i class="fa fa-table"></i><a href="tables-data.html">order1</a></li>
+                    
+                    
+                </ul>
+            </li>
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Invoice</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="menu-icon fa fa-th"></i><a href="forms-basic.html">invoice1</a></li>
+                    <li><i class="menu-icon fa fa-th"></i><a href="forms-advanced.html">invoice1</a></li>
+                </ul>
+            </li>
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Activites</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="menu-icon fa fa-th"></i><a href="<?php echo site_url('Activity/ActiveF')?>">activ</a></li>
+                    <li><i class="menu-icon fa fa-th"></i><a href="forms-advanced.html">activ</a></li>
+                </ul>
+            </li>
+
+            <li class="menu-title">Employee Section</li><!-- /.menu-title -->
+
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Employee</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="menu-icon fa fa-fort-awesome"></i><a href="font-fontawesome.html">Employee1</a></li>
+                    <li><i class="menu-icon ti-themify-logo"></i><a href="font-themify.html">Employee1</a></li>
+                    <li><i class="menu-icon ti-themify-logo"></i><a href="font-themify.html">Employee1</a></li>
+                </ul>
+            </li>
+            
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-bar-chart"></i>Roles</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="menu-icon fa fa-line-chart"></i><a href="charts-chartjs.html">Chart JS</a></li>
+                    <li><i class="menu-icon fa fa-area-chart"></i><a href="charts-flot.html">Flot Chart</a></li>
+                    <li><i class="menu-icon fa fa-pie-chart"></i><a href="charts-peity.html">Peity Chart</a></li>
+                </ul>
+            </li>
+
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-area-chart"></i>Keyword</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="menu-icon fa fa-map-o"></i><a href="maps-gmap.html">Google Maps</a></li>
+                    <li><i class="menu-icon fa fa-street-view"></i><a href="maps-vector.html">Vector Maps</a></li>
+                </ul>
+            </li>
+
+            <li class="menu-title">Documents Section</li><!-- /.menu-title -->
+
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Regulation</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="menu-icon fa fa-fort-awesome"></i><a href="font-fontawesome.html">reg</a></li>
+                    <li><i class="menu-icon ti-themify-logo"></i><a href="font-themify.html">reg</a></li>
+                    <li><i class="menu-icon ti-themify-logo"></i><a href="font-themify.html">reg</a></li>
+                </ul>
+            </li>
+            
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-bar-chart"></i>Certification</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="menu-icon fa fa-line-chart"></i><a href="charts-chartjs.html">Chart JS</a></li>
+                    <li><i class="menu-icon fa fa-area-chart"></i><a href="charts-flot.html">Flot Chart</a></li>
+                    <li><i class="menu-icon fa fa-pie-chart"></i><a href="charts-peity.html">Peity Chart</a></li>
+                </ul>
+            </li>
+
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-area-chart"></i>Service</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="menu-icon fa fa-map-o"></i><a href="maps-gmap.html">Google Maps</a></li>
+                    <li><i class="menu-icon fa fa-street-view"></i><a href="maps-vector.html">Vector Maps</a></li>
+                </ul>
+            </li>
+            <li class="menu-title">Extras</li><!-- /.menu-title -->
+            <li class="menu-item-has-children dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Pages</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li><i class="menu-icon fa fa-sign-in"></i><a href="page-login.html">Login</a></li>
+                    <li><i class="menu-icon fa fa-sign-in"></i><a href="page-register.html">Register</a></li>
+                    <li><i class="menu-icon fa fa-paper-plane"></i><a href="pages-forget.html">Forget Pass</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div><!-- /.navbar-collapse -->
+</nav>
